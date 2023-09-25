9031071851922454a4e87043a618eb241a4941cc
Dasha Agapenkova <93427597+DashaAga@users.noreply.github.com>
2023-09-25 16:50:44 +0300

    main

---
+++ streamlit.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import streamlit as st
+from PIL import Image
+from workWithModel import load_model_and_predict
+
+def process_main_page():
+    show_main_page()
+    process_inputs()
+
+def show_main_page():
+    image = Image.open('data/scoring.jpg')
+
+    st.set_page_config(
+        layout="wide",
+        page_title="Review Rating",
+        page_icon=image,
+
+    )
+
+    st.write(
+        """
+        # Оставьте свой отзыв и получите его в числовом эквиваленте!
+        """
+    )
+
+    st.image(image)
+
+
+def write_prediction(prediction):
+    st.write("## Предсказание")
+    st.write(prediction)
+
+
+def process_inputs():
+    st.write('Оставьте ваш отзыв')
+    text = st.sidebar.text_input(value="Ваш отзыв")
+    prediction = load_model_and_predict(text)
+    write_prediction(prediction)
+
+
+if __name__ == "__main__":
+    process_main_page()
---
+++ workWithModel.py
@@ -0,0 +1,27 @@
+from pickle import load
+import pandas as pd
+
+def text_to_vector(text, vector_size = 6967):
+    # Создаем объект TF-IDF векторизатора с заданным размером
+    vectorizer = TfidfVectorizer(max_features=vector_size, lowercase=True, stop_words='english')
+
+    # Преобразуем текст в TF-IDF вектор
+    tfidf_vector = vectorizer.fit_transform([text])
+
+    # Преобразуем разреженную матрицу в плотный массив
+    dense_vector = tfidf_vector.toarray()
+
+    # Если размер вектора меньше заданного, дополним нулями
+    if dense_vector.shape[1] < vector_size:
+        padding = np.zeros((1, vector_size - dense_vector.shape[1]))
+        dense_vector = np.hstack((dense_vector, padding))
+
+    return dense_vector
+
+def load_model_and_predict(text, path="data/model.pickle"):
+    with open(path, "rb") as file:
+        model = load(file)
+
+    prediction = model.predict(text)
+
+    return f"Вы бы поставили оценку {prediction}"
